3b9dfce91193f09283b8f7e31b21e40fd97b8023
wcqrwtqr <mohalbatati@hotmail.com>
2022-08-08 21:24:10 +0300

    update bash

--- new_os_setup.sh
+++ new_os_setup.sh
@@ -10,3 +10,8 @@ sudo apt install vim tmux emacs zsh
 # installing oh my zsh
 echo "Installing oh my zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo "Add the following line to the zsh plugin"
+echo "zsh-autosuggestions"
